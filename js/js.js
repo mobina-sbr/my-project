0bd6efdce695047487b761fc589517663e5d256a
mobina-sbr <mobinasaberinasab@gmail.com>
2023-10-28 15:13:46 +0330

    added new files

--- js/js.js
+++ js/js.js
@@ -1,8 +1,16 @@
-const baner = document.querySelector('.baner')
+const baner = document.querySelector('.baner');
+const form = document.querySelector('form');
+const input = document.querySelector('input')
 
 
 window.addEventListener('load',function(){
     setTimeout(()=>{
         baner.style.top = '-100vh'
     },2000)
+})
+
+
+form.addEventListener('submit' , event =>{
+    event.preventDefault();
+    console.log(input.value);
 })
